557a421936f41b7b449f592a2809d0f9be226441
Godson Rapture Chijioke <me.rapid79@gmail.com>
2017-08-23 07:58:15 +1200

    Add files via upload
    Source code

---
+++ turtle_keyboard_control.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  4 21:09:17 2017
+
+@author: RAPTURE C. GODSON
+
+for more tutorials, go to www.coolpythoncodes.com
+
+"""
+from turtle import Turtle
+t=Turtle()
+
+def up():
+    if not(t.heading() == 90):
+        t.setheading(90)
+        t.fd(50)
+    else:
+        t.fd(50)
+    
+def down():
+    if not(t.heading() == 270):
+        t.setheading(270)
+        t.fd(50)
+    else:
+        t.fd(50)
+    
+def right():
+    if not (t.heading() == 0):
+        t.setheading(0)
+        t.fd(50)
+    else:
+        t.fd(50)
+    
+def left():
+    if not (t.heading() ==180):
+        t.setheading(180)
+        t.fd(50)
+    else:
+        t.fd(50)
+
+def undo_button():
+    t.undo()
+        
+def keyboard_commands():
+    t.screen.onkey(up,"Up")
+    t.screen.onkey(down,"Down")
+    t.screen.onkey(right,"Right")
+    t.screen.onkey(left,"Left")
+    t.screen.onkey(undo_button,"End")
+    t.screen.listen()
+
+keyboard_commands()
+t.screen.mainloop()
